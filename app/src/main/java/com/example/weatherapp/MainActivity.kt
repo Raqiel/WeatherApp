8893dd1f80d7834ffa4f53339dd79cb86c69e2f8
Raquel de Oliveira Gonçalves <raquel@bunker79.com>
2022-10-20 21:12:15 -0300

    Search for Betim

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.adapter.WeatherAdapter
+import com.example.weatherapp.model.util.Constants
+import com.example.weatherapp.model.util.Constants.Companion.WEATHER_API_BASE_URL
+import com.example.weatherapp.model.util.IWeatherEndPoints
+import com.example.weatherapp.model.util.NetWorkUtils
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var rvWeather : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rvWeather = findViewById(R.id.rvWeather)
+
+        searchForecast("Betim")
+    }
+
+    private fun searchForecast(city : String) {
+        val network = NetWorkUtils.getRetrofitInstance(Constants.WEATHER_API_BASE_URL)
+        val endpoint = network.create(IWeatherEndPoints::class.java)
+
+        endpoint.requestWeatherForecast(city, 3)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                val weatherData = it.forecast.ForecastDay
+                rvWeather.adapter = WeatherAdapter(weatherData)
+            },
+                {
+                    Log.d(localClassName, "Error ${it.message}")
+                })
+
     }
 }
+
+
--- app/src/main/java/com/example/weatherapp/model/util/IWeatherEndPoints.kt
+++ app/src/main/java/com/example/weatherapp/model/util/IWeatherEndPoints.kt
@@ -8,9 +8,9 @@ import retrofit2.http.Query
 
 interface IWeatherEndPoints {
     @Headers(
-        "X-RapidAPI-Host : weatherapi-com.p.rapidapi.com",
-        "X-RapidAPI-Key : 463c34412cmsh4569459a7e1c758p1cc025jsn8c26951c3229"
+        "X-RapidAPI-Host:weatherapi-com.p.rapidapi.com",
+        "X-RapidAPI-Key:463c34412cmsh4569459a7e1c758p1cc025jsn8c26951c3229"
     )
     @GET("forecast.json")
-    fun requestWeatherForecast(@Query ("q") city : String, @Query("days") numberOfDays: Int) : Maybe<WeatherForecastResult>
+    fun requestWeatherForecast(@Query("q") city : String, @Query("days") numberOfDays: Int) : Maybe<WeatherForecastResult>
 }
